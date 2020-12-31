054252ee561811c88652457717882055ff2fc314
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-12-31 15:41:25 -0300

    first commit

---
+++ woocommerce-shipped-status.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Shipped Status
+ * Version: 1.0.0
+ * Description: Adds the Shipped status in the customer's orders
+ * Author: Douglas Fanucchi
+ * Author URI: https://fanucchi.dev
+
+ * Text Domain: woocommerce-shipped-status
+ * Domain Path: /languages
+
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
