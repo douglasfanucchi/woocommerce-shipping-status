c9839a9153f19ada28c1b7914373818e77e60287
Douglas Fanucchi <douglasfanucchi@gmail.com>
2021-01-02 22:44:50 -0300

    adds front end assets

--- src/order-status-tracking.class.php
+++ src/order-status-tracking.class.php
@@ -5,10 +5,15 @@ if ( ! class_exists( 'Fanucchi\\FN_Order_Status_Tracking' ) ) {
 	class FN_Order_Status_Tracking {
 		public function __construct() {
 			$this->add_shortcode();
+			$this->add_actions();
 		}
 
 		private function add_shortcode() {
 			\add_shortcode( 'fn_status_tracking', array( $this, 'generate_shortcode' ) );
+		}
+
+		private function add_actions() {
+			\add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 		}
 
 		public function generate_shortcode( $args ) {
@@ -26,6 +31,11 @@ if ( ! class_exists( 'Fanucchi\\FN_Order_Status_Tracking' ) ) {
 				'',
 				\FN_SHIPPED_STATUS_TEMPLATES_PATH
 			);
+		}
+
+		public function enqueue_scripts() {
+			\wp_enqueue_script( 'fn_tracking_order_status', \FN_SHIPPED_STATUS_URL . 'public/js/index.js', array( 'jquery' ) );
+			\wp_enqueue_style( 'fn_tracking_order_status', \FN_SHIPPED_STATUS_URL . 'public/css/index.css' );
 		}
 	}
 }
